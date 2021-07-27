b5831e6b33e5f50cb38f5d6edc44fc3a752e58f3
GreatPattra <greatpatpat@gmail.com>
2021-07-27 20:15:37 +0700

    intial commit

--- HWsticks_640631126.py
+++ HWsticks_640631126.py
@@ -12,6 +12,16 @@ def python(take_stick):
     take_stick = 3 - take_stick
     print("I. smart computer, take", take_stick )
     return take_stick
+
+def p1take(sticks):
+    if (sticks + 2)%3 == 0 :
+       take_stick = random1()               
+    if (sticks + 2)%3 == 1 :
+       take_stick = 1 
+    if (sticks + 2)%3 == 2 :
+       take_stick = 2 
+    print("I, smart computer, take", take_stick ) 
+    return take_stick
     
 def main():
   player_turn = 1
@@ -25,18 +35,12 @@ def main():
          if sticks > 1 :  
               print("Turn: Player", player_turn)
               if player_turn == 1:
-                  if (sticks + 2)%3 == 0 :
-                     take_stick = random1()               
-                  if (sticks + 2)%3 == 1 :
-                     take_stick = 1 
-                  if (sticks + 2)%3 == 2 :
-                     take_stick = 2 
-                  print("I. smart computer, take", take_stick ) 
+                  take_stick = p1take(sticks)
               if player_turn == 2:  
                   take_stick = int(input(str(name) + ", How many sticks do you want to take? (1 or 2): "))
-              if take_stick < 1 or take_stick > 2:
-                  print("Sorry, that's not a valid number.")
-                  continue
+                  if take_stick < 1 or take_stick > 2:
+                      print("Sorry, that's not a valid number.")
+                      continue
               sticks -= take_stick 
               print("There are", sticks, "sticks in the pile.")
               player_turn += 1
@@ -51,11 +55,11 @@ def main():
                  print(str(name) + ", wins! (I, smart computer, am sad ;---;)") 
              if player_turn == 2:     
                  take_stick = int(input(str(name) + ", How many sticks do you want to take? (1 or 2): "))       
-             if take_stick < 1 or take_stick > 1:
-                 print("Sorry, that's not a valid number.", "\n")
-                 continue
-             print(str(name) + ", takes the last stick.", "\n")
-             print("I, smart computer, wins! >-<")             
+                 if take_stick < 1 or take_stick > 1:
+                     print("Sorry, that's not a valid number.", "\n")
+                     continue
+                 print(str(name) + ", takes the last stick.", "\n")
+                 print("I, smart computer, wins! >-<")             
              break        
          
   else :
@@ -63,13 +67,7 @@ def main():
          if sticks > 1 :  
               print("Turn: Player", player_turn)
               if player_turn == 1:
-                  if (sticks + 2)%3 == 0 :
-                     take_stick = random1()               
-                  if (sticks + 2)%3 == 1 :
-                     take_stick = 1 
-                  if (sticks + 2)%3 == 2 :
-                     take_stick = 2 
-                  print("I. smart computer, take", take_stick ) 
+                  take_stick = p1take(sticks)
               if player_turn == 2:  
                   take_stick = int(input(str(name) + ", How many sticks do you want to take? (1 or 2): "))
               if take_stick < 1 or take_stick > 2:
@@ -89,11 +87,11 @@ def main():
                  print(str(name) + ", wins! (I, smart computer, am sad ;---;)") 
              if player_turn == 2:     
                  take_stick = int(input(str(name) + ", How many sticks do you want to take? (1 or 2): "))       
-             if take_stick < 1 or take_stick > 1:
-                 print("Sorry, that's not a valid number.", "\n")
-                 continue
-             print(str(name) + ", takes the last stick.", "\n")
-             print("I, smart computer, wins! >-<")             
+                 if take_stick < 1 or take_stick > 1:
+                     print("Sorry, that's not a valid number.", "\n")
+                     continue
+                 print(str(name) + ", takes the last stick.", "\n")
+                 print("I, smart computer, wins! >-<")             
              break 
 
 main()
